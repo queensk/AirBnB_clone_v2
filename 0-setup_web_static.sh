f6fb536531bd9d2efdeadc0cdcdd590e1fead9b7
queensk <queenskisivuli@gmail.com>
2023-01-24 17:42:32 +0300

    updatedd the test output

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployment of web_static
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install nginx
-if [ ! -d "/data/web_static/releases/test" ] || [ ! -d "/data/web_static/shared" ]; then
-    sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-fi
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
