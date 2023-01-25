7b3084bd3f1a46be3b874314593d6ee278dc871f
queensk <queenskisivuli@gmail.com>
2023-01-25 17:01:23 +0300

    refactor Prepare your web servers using puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -21,54 +21,55 @@ $nginx_conf = "server {
     }
 }"
 
-package { "nginx":
-    ensure => "installed",
-    provider => apt,
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
 } ->
 
-file { "/data":
-    ensure => "directory",
+file { '/data':
+  ensure  => 'directory'
 } ->
 
-file { ['/data/web_static', '/data/web_static/releases', '/data/web_static/shared', '/data/web_static/releases/test']:
-    ensure => "directory",
+file { ['/data/web_static', '/data/web_static/releases',
+        '/data/web_static/releases/test', '/data/web_static/shared']:
+  ensure => 'directory'
 } ->
 
 file { '/data/web_static/releases/test/index.html':
-    ensure => 'present',
-    contents => 'Holberton School Puppet\n',    
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
 } ->
 
 file { '/data/web_static/current':
-    ensure => 'link',
-    target => 'data/web_static/releases/test',
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
 } ->
 
-file { 'chown -R ubuntu:ubuntu /data/':
-    path => '/usr/bin/:/usr/local/bin/:/bin/',
-} ->
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
 
 file { '/var/www':
-    ensure => 'directory'
+  ensure => 'directory'
 } ->
 
 file { '/var/www/html':
-    ensure => 'directory',
+  ensure => 'directory'
 } ->
 
 file { '/var/www/html/index.html':
-    ensure => 'present',
-    content => 'Holberton School Nginx\n',
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
 } ->
 
 file { '/var/www/html/404.html':
-    ensure => 'present',
-    content => 'Ceci n'est pas une page\n',
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
 } ->
 
 file { '/etc/nginx/sites-available/default':
-    ensure => 'present',
-    content => $nginx_config,
+  ensure  => 'present',
+  content => $nginx_conf
 } ->
 
 exec { 'nginx restart':
