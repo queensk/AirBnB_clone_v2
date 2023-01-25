e80d07a43491afdd6cbca50734f58445d96aabc3
queensk <queenskisivuli@gmail.com>
2023-01-25 16:33:34 +0300

    Prepare your web servers using puppet

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,76 @@
+# setup and configure a web sever
+
+# configure nginx
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { "nginx":
+    ensure => "installed",
+    provider => apt,
+} ->
+
+file { "/data":
+    ensure => "directory",
+} ->
+
+file { ['/data/web_static', '/data/web_static/releases', '/data/web_static/shared', '/data/web_static/releases/test']:
+    ensure => "directory",
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+    ensure => 'present',
+    contents => 'Holberton School Puppet\n',    
+} ->
+
+file { '/data/web_static/current':
+    ensure => 'link',
+    target => 'data/web_static/releases/test',
+} ->
+
+file { 'chown -R ubuntu:ubuntu /data/':
+    path => '/usr/bin/:/usr/local/bin/:/bin/',
+} ->
+
+file { '/var/www':
+    ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+    ensure => 'directory',
+} ->
+
+file { '/var/www/html/index.html':
+    ensure => 'present',
+    content => 'Holberton School Nginx\n',
+} ->
+
+file { '/var/www/html/404.html':
+    ensure => 'present',
+    content => 'Ceci n'est pas une page\n',
+} ->
+
+file { '/etc/nginx/sites-available/default':
+    ensure => 'present',
+    content => $nginx_config,
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
+}
