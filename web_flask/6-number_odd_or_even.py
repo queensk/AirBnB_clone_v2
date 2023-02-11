2eda997478fd4aa99405256119084ca7f184ff4b
queensk <queenskisivuli@gmail.com>
2023-02-11 04:05:22 +0300

    Number: n is even|odd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -68,5 +68,7 @@ def oddEven(n):
         number = "odd"
     return render_template('6-number_odd_or_even.html', n=n,
                            number=number)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port='5000')
